97be66cd1c46d2f3e7f41ff486e4ffd9bdaece29
Juan-Pierre DE BEER <jde-beer@c4r18s4.wethinkcode.co.za>
2019-07-22 15:50:19 +0200

    gnl

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jde-beer <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/12 13:57:07 by jde-beer          #+#    #+#             */
+/*   Updated: 2019/07/12 15:06:04 by jde-beer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "libft/libft.h"
+# include <unistd.h>
+
+# define BUFF_SIZE 32
+
+int		get_next_line(const int fd, char **line);
+
+#endif
